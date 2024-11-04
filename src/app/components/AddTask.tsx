c9e79b1aac842a507515d5902ecbadb98fa77be8
majimajimazzy <j320252@g.kogakuin.jp>
2024-11-05 00:00:08 +0900

    1105-0000

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -3,12 +3,13 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import { addTodo } from "../api/api";
 import { todo } from "node:test";
+import {v4 as uuidv4 } from "uuid";
 
 const AddTask = () => {
   const [taskTitle, setTaskTitle] = useState("")
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    await addTodo({id: "4", text: taskTitle});
+    await addTodo({id: uuidv4(), text: taskTitle});
     setTaskTitle("");
   };
   return (
--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -1,17 +1,61 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 import { Task } from "../api/types";
+import { deleteTodo, editTodo } from "../api/api";
 
 interface TodoProps{
   todo: Task;
-}
+};
 
 const Todo = ({todo}:TodoProps) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTaskTitle,setEditedTaskTitle] = useState(todo.text);
+
+  useEffect(() => {
+    if(isEditing)
+    {
+      ref.current?.focus();
+    }
+  },[isEditing]);
+
+  const handleEdit = async () => {
+    setIsEditing(true);
+  };
+  const handleSave = async () => {
+    await editTodo(todo.id, editedTaskTitle);
+    setIsEditing(false);
+  };
+  const handleDelete = async () => {
+    await deleteTodo(todo.id);
+  };
   return (
     <li key={todo.id} className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>{todo.text}</span>
-        <div>
-          <button className="text-green-500 mr-3">Edit</button>
-          <button className="text-red-500">Delete</button>
+        {isEditing ? (<input
+        ref = {ref}
+        type="text" 
+        className="mr-2 py-1 px-2 rounded birder-gray-400 border"
+        value={editedTaskTitle}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setEditedTaskTitle(e.target.value)
+        }
+        /> ): (
+          <span>{todo.text}</span>
+        )}
+        <div className="flex">
+          {isEditing ? (
+            <button className="text-blue-500 mr-3" onClick={handleSave} >
+              Save
+            </button>
+          ) : (
+            <button className="text-green-500 mr-3" onClick={handleEdit} >
+              Edit
+            </button>
+          )}
+          
+          <button className="text-red-500" onClick={handleDelete} >Delete</button>
         </div>
       </li>
   );
