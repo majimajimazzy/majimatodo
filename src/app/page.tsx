feba37fdec5e0d176fc8815f91173d2342eeada0
majimajimazzy <j320252@g.kogakuin.jp>
2024-11-04 19:29:02 +0900

    1104-1930

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { getAllTodos } from "./api/api";
 
 export default async function Home() {
   const todos = await getAllTodos();
-  console.log(todos);
   return(
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
       <h1 className="text-4xl font-bold text-gray-700 -mt-32">
@@ -14,7 +13,7 @@ export default async function Home() {
       <div className="w-full max-w-xl mt-5">
         <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
           <AddTask />
-          <TodoList />
+          <TodoList todos = {todos}/>
         </div>
       </div>
     </main>
