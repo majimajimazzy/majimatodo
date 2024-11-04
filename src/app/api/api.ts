f9449fec654a6c42902599dddcce88bdb1452b5e
majimajimazzy <j320252@g.kogakuin.jp>
2024-11-04 19:56:27 +0900

    1104-2000

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -5,3 +5,15 @@ export const getAllTodos = async (): Promise<Task[]> => {
   const todos = res.json();
   return todos;
 };
+
+export const addTodo = async (todo: Task): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/tasks`,{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodo = res.json();
+  return newTodo;
+};
